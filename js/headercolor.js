b834a5c0f54338ea93fbf73983ad5661577fc204
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-09-22 23:24:51 +0900

    update

--- js/headercolor.js
+++ js/headercolor.js
@@ -3,7 +3,7 @@ const headerHeight = header.getBoundingClientRect().height;
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > headerHeight) {
-    header.style.backgroundColor = "#d6f1f0";
+    header.style.backgroundColor = "#fff";
     header.style.transition = "background-color 1s ease"; // 부드러운 변화를 위한 transition 설정
   } else {
     header.style.backgroundColor = "transparent";
